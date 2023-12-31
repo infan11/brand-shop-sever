f2191a9d57b5a1ea52978905b503fdcd9d9476c3
infan11 <infanjiounrahman20606@gmail.com>
2023-12-31 22:21:05 +0600

    added index.js all file

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ app.get("/products/:id" , async(req, res) =>{
 // update 
 app.put("/products/:id" , async(req, res) =>{
   const id = req.params.id;
+  console.log(id);
   const filter = {_id: new ObjectId(id)}
   const option = {upsert : true};
   const  updateProduct = req.body;
@@ -69,7 +70,13 @@ app.post("/products" , async (req, res) =>{
  const result = await productsCollection.insertOne(product)
  res.send(result)
 })
-/
+// my cart deleted
+app.delete("/products/:id" , async(req, res) =>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)}
+  const result = await productsCollection.deleteOne(query)
+  res.send(result)
+})
 
 
 // Send a ping to confirm a successful connection
